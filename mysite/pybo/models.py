2d7659f7febe92e8e1ff730eeb80c94aeee801a3
양경열 <darkdarkget@naver.com>
2023-06-22 18:18:10 +0900

    add models

--- mysite/pybo/models.py
+++ mysite/pybo/models.py
@@ -1,3 +1,49 @@
 from django.db import models
+from django.contrib.auth.models import User
+class article(models.Model):
+    #article 테이블
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_article')
+    #제목
+    subject = models.CharField(max_length=200)
+    #모집 운동 종목
+    sport_type = models.CharField(max_length=20)
+    #모집 성별
+    gender = models.CharField(max_length=200)
+    #모집 상태
+    status = models.CharField(max_length=3)
+    #모집 지역
+    area = models.CharField(max_length=200)
+    #운동 할 날
+    sport_date = models.DateTimeField() #함수 참조 요망
+    #모집 내용
+    content =models.TextField()
 
-# Create your models here.
+    create_date = models.DateTimeField()
+    #유저 테이블에 링크가된다 .
+    modify_date = models.DateTimeField(null=True,blank=True)
+    voter = models.ManyToManyField(User,related_name='voter_article') #voter추가
+    def __str__(self):
+        return self.subject
+
+# class content(models.Model):
+#     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_content')
+#     question = models.ForeignKey(article, on_delete=models.CASCADE)
+#     #ForeignKey는 외례키를 의미한다.
+#     content = models.TextField()
+#     create_date = models.DateTimeField()
+#
+#     #CASCADE는 계정이 삭제되면 연결된 모델도 삭제
+#     modify_date = models.DateTimeField(null=True, blank=True)
+#     voter = models.ManyToManyField(User,related_name='voter_content')
+
+class Comment(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    create_date = models.DateTimeField()
+    modify_date = models.DateTimeField(null=True, blank=True)
+    article = models.ForeignKey(article, null=True,blank=True,on_delete=models.CASCADE)
+    #answer = models.ForeignKey(content,null=True,blank=True,on_delete=models.CASCADE)
+
+
+
+
