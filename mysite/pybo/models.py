7fd4f9cef1fef903a9bcff03918ad8b9e4ed7682
양경열 <darkdarkget@naver.com>
2023-06-26 12:18:20 +0900

    setting

--- mysite/pybo/models.py
+++ mysite/pybo/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from common.models import User
-class article(models.Model):
+
+
+
+class Article(models.Model):
     #article 테이블
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_article')
     #제목
@@ -25,28 +28,7 @@ class article(models.Model):
     def __str__(self):
         return self.subject
 
-# class content(models.Model):
-#     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_content')
-#     question = models.ForeignKey(article, on_delete=models.CASCADE)
-#     #ForeignKey는 외례키를 의미한다.
-#     content = models.TextField()
-#     create_date = models.DateTimeField()
-#
-#     #CASCADE는 계정이 삭제되면 연결된 모델도 삭제
-#     modify_date = models.DateTimeField(null=True, blank=True)
-#     voter = models.ManyToManyField(User,related_name='voter_content')
 
-class Comment(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    content = models.TextField()
-    create_date = models.DateTimeField()
-    modify_date = models.DateTimeField(null=True, blank=True)
-    article = models.ForeignKey(article, null=True,blank=True,on_delete=models.CASCADE)
-    #answer = models.ForeignKey(content,null=True,blank=True,on_delete=models.CASCADE)
-
-#수정중
-class pro(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
 
 
 
--- mysite/pybo/views/base_views.py
+++ mysite/pybo/views/base_views.py
@@ -1,7 +1,27 @@
-from django.shortcuts import render
+from django.core.paginator import Paginator
+from django.shortcuts import render, get_object_or_404, redirect
+from django.db.models import Q,Count
+
 
 # Create your views here.
 
 
 def main(request):
     return render(request,'pybo/main.html',)
+
+
+def article_detail(request,question_id):
+    page = request.GET.get('page', '1')
+    #request에서 숫자를 호출받는다 ...
+    # question=Question.objects.get(id=question_id)
+    question = get_object_or_404(Question,pk=question_id)
+    #페이징을 위한 index
+    answer_list = question.answer_set.order_by('-voter','-create_date')
+    #페이징 처리
+    paginator = Paginator(answer_list,1)#페이지당 1개씩 보이기
+    page_obj = paginator.get_page(page)
+
+
+    #get_object(Question)=> Question.object.get의 의미
+    context={'question':question,'answer_list': page_obj}
+    return render(request,'pybo/question_detail.html',context)
