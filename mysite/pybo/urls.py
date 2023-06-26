fe0f0b5187598985072a6a90480eaea37f9df3b1
양경열 <darkdarkget@naver.com>
2023-06-26 17:12:40 +0900

    main_page_ing#2

--- mysite/pybo/urls.py
+++ mysite/pybo/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 from .views import base_views,article_views
 
 app_name = 'pybo'
-
+#article까지 진행
 urlpatterns =[
     path('',base_views.main,name='main'),
     path('article/create/',article_views.article_create,name='article_create'),
+
 
 ]
