39a34fd0558449f0da95200117941d2d42cb5193
양경열 <darkdarkget@naver.com>
2023-07-04 16:49:56 +0900

    update_master

--- mysite/pybo/views/base_views.py
+++ mysite/pybo/views/base_views.py
@@ -1,7 +1,7 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404, redirect
 from django.db.models import Q,Count
-from ..models import Article
+from ..models import Article,ArticleComment
 from common.models import Profile
 
 
@@ -20,7 +20,7 @@ def article_index(request):
     # question_list = Question.objects.order_by('-create_date')
 
 
-    article_list = Article.objects.order_by('create_date')
+    article_list = Article.objects.order_by('-create_date')
     if kw:
         article_list = article_list.filter(
             Q(subject__icontains=kw) |  # 제목검색
@@ -33,3 +33,24 @@ def article_index(request):
     context = {'article_list': page_obj, 'profile':profile,'page':page,'kw':kw}
 
     return render(request, 'pybo/article_list.html' ,context)
+
+def main_index(request):
+    page = request.GET.get('page','1')
+    kw = request.GET.get('kw','')#검색어
+
+
+
+    article_list = Article.objects.order_by()
+    if kw:
+        article_list = article_list.filter(
+            Q(subject__icontains=kw) |  # 제목검색
+            Q(content__icontains=kw)  # 내용검색
+        ).distinct()
+    profile = Profile.objects
+    #페이징 처리
+    paginator = Paginator(article_list,3)#페이지당 1개씩 보이기
+    page_obj = paginator.get_page(page)
+    context = {'article_list': page_obj, 'profile':profile,'page':page,'kw':kw}
+
+    return render(request, 'pybo/article_list.html' ,context)
+
