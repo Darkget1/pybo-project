6e7e0bfa1fdeff2a4e3191e090ba1ba0aaa6da3f
darkdarkget <darkdarkget@naver.com>
2023-07-04 23:27:51 +0900

    profile_img &profile_select

--- test/test1.py
+++ test/test1.py
@@ -1,6 +1,15 @@
-a=[1,1,3,4,5]
+import requests
+def elec_location(region,page_num):
+    url = 'https://dapi.kakao.com/v2/local/search/keyword.json'
+    params = {'query': region,'page': page_num}
+    headers = {"Authorization": "e7c76ca9e8140eba8ebfe7423b791241"}
 
-for b in a:
-    if (b==1).count==1:
-        print(b)
+    places = requests.get(url, params=params, headers=headers).json()['documents']
+    total = requests.get(url, params=params, headers=headers).json()['meta']['total_count']
+    if total > 45:
+        print(total,'개 중 45개 데이터밖에 가져오지 못했습니다!')
+    else :
+        print('모든 데이터를 가져왔습니다!')
+    return places
 
+print(elec_location('강남역',1))
